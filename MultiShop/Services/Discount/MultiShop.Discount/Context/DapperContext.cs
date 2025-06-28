4872dbffb1c689f4c65196ad66d94d1b7788ca1f
Fırat Alçın <firatalcin04@gmail.com>
2025-06-28 14:46:36 +0300

    Discount Migration

--- MultiShop/Services/Discount/MultiShop.Discount/Context/DapperContext.cs
+++ MultiShop/Services/Discount/MultiShop.Discount/Context/DapperContext.cs
@@ -1,8 +1,27 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MultiShop.Discount.Entities;
+using System.Data;
 
 namespace MultiShop.Discount.Context
 {
     public class DapperContext : DbContext
     {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=MAKINA\\SQLEXPRESS01; database= MultiShopDiscountDb; integrated security=true;TrustServerCertificate=True");
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+        public IDbConnection CreateConnection => new SqlConnection(_connectionString);
     }
 }
