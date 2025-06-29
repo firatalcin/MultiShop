79823c25f21592abf4d9495c965d6feda6fe44cb
Fırat Alçın <firatalcin04@gmail.com>
2025-06-29 21:25:33 +0300

    Order Domain Katmanı

--- MultiShop/Services/Order/Core/MultiShop.Order.Domain/Entities/Address.cs
+++ MultiShop/Services/Order/Core/MultiShop.Order.Domain/Entities/Address.cs
@@ -8,5 +8,10 @@ namespace MultiShop.Order.Domain.Entities
 {
     public class Address
     {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string District { get; set; }
+        public string City { get; set; }
+        public string Detail { get; set; }
     }
 }
