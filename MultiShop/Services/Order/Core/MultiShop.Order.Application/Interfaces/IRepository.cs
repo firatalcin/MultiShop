71b5740276a367ac0c5946820bcc38294bc74c02
Fırat Alçın <firatalcin04@gmail.com>
2025-07-19 16:54:52 +0300

    Get Address List and Get Address By Id Handler

--- MultiShop/Services/Order/Core/MultiShop.Order.Application/Interfaces/IRepository.cs
+++ MultiShop/Services/Order/Core/MultiShop.Order.Application/Interfaces/IRepository.cs
@@ -10,10 +10,10 @@ namespace MultiShop.Order.Application.Interfaces
     public interface IRepository<T> where T : class
     {
         Task<List<T>> GetAllAsync();
-        Task<T> GetByIdAsync(Guid id);
+        Task<T> GetByIdAsync(int id);
         Task<T> CreateAsync(T entity);
         Task<T> UpdateAsync(T entity);
-        Task<T> DeleteAsync(Guid id);
+        Task<T> DeleteAsync(int id);
         Task<T> GetByFilterAsync(Expression<Func<T, bool>> filter);
     }
 }
