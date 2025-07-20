bf206e88a593bc415671f35fc5af13a02504a577
Fırat Alçın <firatalcin04@gmail.com>
2025-07-20 20:22:10 +0300

    Ordering Processes With Mediator

--- MultiShop/Services/Order/Core/MultiShop.Order.Application/Features/Mediator/Commands/OrderingCommands/CreateOrderingCommand.cs
+++ MultiShop/Services/Order/Core/MultiShop.Order.Application/Features/Mediator/Commands/OrderingCommands/CreateOrderingCommand.cs
@@ -1,17 +1,16 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MultiShop.Order.Domain.Entities
+namespace MultiShop.Order.Application.Features.Mediator.Commands.OrderingCommands
 {
-    public class Ordering
+    public class CreateOrderingCommand : IRequest
     {
-        public int Id { get; set; }
         public string UserId { get; set; }
         public decimal TotalPrice { get; set; }
         public DateTime OrderDate { get; set; }
-        public List<OrderDetail> OrderDetails { get; set; }
     }
 }
--- MultiShop/Services/Order/Core/MultiShop.Order.Application/Features/Mediator/Results/OrderingResults/GetOrderingQueryResult.cs
+++ MultiShop/Services/Order/Core/MultiShop.Order.Application/Features/Mediator/Results/OrderingResults/GetOrderingQueryResult.cs
@@ -4,14 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MultiShop.Order.Domain.Entities
+namespace MultiShop.Order.Application.Features.Mediator.Results.OrderingResults
 {
-    public class Ordering
+    public class GetOrderingQueryResult
     {
         public int Id { get; set; }
         public string UserId { get; set; }
         public decimal TotalPrice { get; set; }
         public DateTime OrderDate { get; set; }
-        public List<OrderDetail> OrderDetails { get; set; }
     }
 }
